369ef84bf8c66593d49d4291952c5ddb558b9e6d
sfloresmeza <santiagofloresmeza@gmail.com>
2023-12-06 15:17:28 -0700

    Adding styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import styles from './styles'
 
 const App = () => (
-  <div className="bg-primary w-full overflow-hidden">
-    <h1>Hello World!</h1>
+  <div className='bg-primary w-full overflow-hidden'>
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        Navbar
+      </div>
+    </div>
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        Hero
+      </div>
+    </div>
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        About Us
+        Services
+        Portfolio
+        Contact Us
+        Blog
+        Clients
+        Industries
+        Footer
+      </div>
+    </div>
   </div>
 );
 
